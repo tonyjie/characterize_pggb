fa128c558b8024fef9cdae378183c3dca6e80409
tonyjie <jiajieli0429@gmail.com>
2023-12-09 18:18:31 -0500

    script fo scaling exp

--- script/thread_scaling/scale_wfmash.sh
+++ script/thread_scaling/scale_wfmash.sh
@@ -2,6 +2,12 @@
 
 PANGENOME=$1 # option: "DRB1-3123" or "ecoli50"
 
+# if others, echo error message and exit
+if [ ${PANGENOME} != "DRB1-3123" ] && [ ${PANGENOME} != "ecoli50" ]; then
+    echo "Error: PANGENOME should be DRB1-3123 or ecoli50"
+    exit 1
+fi
+
 
 DATASET_DIR="/work/shared/users/phd/jl4257/Course/CS6230/characterize_pggb/dataset"
 FA_FILE="${DATASET_DIR}/${PANGENOME}/${PANGENOME}.fa.gz"
@@ -14,17 +20,36 @@ if [ ! -d ${LOG_DIR} ]; then
     mkdir -p ${LOG_DIR}
 fi
 
-
 # test run time using different number of threads
 thread_list=(32 16 8 4 2 1)
-set -x
-for num_threads in "${thread_list[@]}";
-do
-    echo "Running wfmash with ${num_threads} threads"
-    { time wfmash \
-        ${FA_FILE} \
-        ${FA_FILE} \
-        --threads ${num_threads} \
-        -n 11 -s 5000 -p 90  -X  -l 25000 -k 19 -H 0.001 > ${PAF_FILE} ; } \
-        2>&1 | tee -a ${LOG_FILE}
-done
+
+# if PANGENOME="DRB1-3123"
+if [ ${PANGENOME} = "DRB1-3123" ]; then
+    # DRB1-3123
+    for num_threads in "${thread_list[@]}";
+    do
+        echo "========= Running wfmash with ${num_threads} threads ========="
+        { time wfmash \
+            ${FA_FILE} \
+            ${FA_FILE} \
+            --threads ${num_threads} \
+            -n 11 -s 5000 -p 90  -X  -l 25000 -k 19 -H 0.001 > ${PAF_FILE} ; } \
+            2>&1 | tee -a ${LOG_FILE}
+    done
+fi
+
+# if PANGENOME="ecoli50"
+if [ ${PANGENOME} = "ecoli50" ]; then
+    # ecoli-50
+    for num_threads in "${thread_list[@]}";
+    do
+        echo "Running wfmash with ${num_threads} threads"
+        { time wfmash \
+            ${FA_FILE} \
+            ${FA_FILE} \
+            --threads ${num_threads} \
+            -n 49 -s 5000 -p 90  -X  -l 25000 -k 19 -H 0.001 > ${PAF_FILE} ; } \
+            2>&1 | tee -a ${LOG_FILE}
+    done
+fi
+
